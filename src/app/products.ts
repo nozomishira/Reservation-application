26ce4027e129b02106ef842adb0b86ae9aba88f2
nozomishira <71810673+nozomishira@users.noreply.github.com>
2021-12-06 19:10:23 +0900

    写真張り付け

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,21 +2,45 @@ export const products = [
   {
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    heading1:'Coz evry night lying a bed',
+    heading2:'a million amillion dreams for the world we are gonna make',
+    heading3:'They can say they can say we have lost our minds',
+    headingtext1:'サンプルテキスト,サンプルテキスト,サンプルテキスト',
+    headingtext2:'sampleText,sampleText,sampleText,sampleText,sampleText',
+    headingtext3:'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
   },
   {
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    heading1:'waiving through a windows ',
+    heading2:'Is any body waiving back at me',
+    heading3:'Is any body waiving, waiving, waiving ... whoh...whoh',
+    headingtext1:'サンプルテキスト,サンプルテキスト,サンプルテキスト',
+    headingtext2:'sampleText,sampleText,sampleText,sampleText,sampleText',
+    headingtext3:'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
   },
   {
     name: 'Phone Standard',
     price: 299,
-    description: ''
+    description: '',
+    heading1:'How do we rewrite the stars',
+    heading2:'This is me',
+    heading3:'No one can rewrite the stars',
+    headingtext1:'サンプルテキスト,サンプルテキスト,サンプルテキスト',
+    headingtext2:'sampleText,sampleText,sampleText,sampleText,sampleText',
+    headingtext3:'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
   },
   {
     name:'phone Special',
     price:999,
-    description:''
+    description:'Its very expensive',
+    heading1:'she screama in silence',
+    heading2:'Twenty guns into the sky you and I',
+    heading3:'Throw up your arms into the sky you and I',
+    headingtext1:'サンプルテキスト,サンプルテキスト,サンプルテキスト',
+    headingtext2:'sampleText,sampleText,sampleText,sampleText,sampleText',
+    headingtext3:'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
   }
 ];
