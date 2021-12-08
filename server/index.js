ee90e96bddbb00792dac006834eed2321b85a292
nozomishira <71810673+nozomishira@users.noreply.github.com>
2021-12-08 16:06:42 +0900

    backinitialize

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/products',function(req,res){
+  res.json({'success':true})
+})
+const PORT = process.env.PORT || '3001'
+
+app.listen('3001',function(){
+  console.log('I am running')
+})
